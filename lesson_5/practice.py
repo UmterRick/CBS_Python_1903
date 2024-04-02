a505ec304910d40caada0dd449aac34927c33606
umterrick <usichenko.vlad67@gmail.com>
2024-04-02 19:30:02 +0300

    lesson 5 practice update

--- lesson_5/practice.py
+++ lesson_5/practice.py
@@ -15,11 +15,36 @@ comment = """
 хотілось спортзал, але приємне обслуговування! Дякую 
 """
 
-triggers = "меню", "спортзал", "обслуговування"
-discount = 0
-if len(comment) > 60:
-    discount += 15
+# triggers = "меню", "спортзал", "обслуговування"
+# discount = 0
+# if len(comment) > 60:
+#     discount += 15
+#
+# for word in triggers:
+#     discount += comment.lower().count(word) * 5
+# print(discount)
 
-for word in triggers:
-    discount += comment.lower().count(word) * 5
-print(discount)
+triggers = ("меню", "спортзал", "обслуговування")
+'''
+Скількі разів повторювались слова тригери в тексті
+за кожне повторення +5%
+Якщо текст >60 символів: + 15%
+'''
+# discount = 0
+# for trigger_word in triggers:
+#     discount_per_word = comment.lower().count(trigger_word) * 5
+#     discount += discount_per_word
+#
+# if len(comment) > 60:
+#     discount += 15
+
+rgb_colour = tuple(input("Set Red, Green, Blue: ").split(","))
+# print(rgb_colour, type(rgb_colour))
+
+possible_range = range(0, 256)
+
+for colour in rgb_colour:
+    if not colour.isdigit() or int(colour) not in possible_range:
+        print("Invalid colour value")
+        exit()
+print(rgb_colour, type(rgb_colour))
